a0bd8d67c67cf36beeec8b066e689d8ed3b6acd5
jrodolforeis <joserodolfomendes@hotmail.com>
2021-06-09 16:33:46 -0300

    Config files
    Gradle, MainActivity, navigation, menus

--- app/src/main/java/com/example/pontosrecife/MainActivity.kt
+++ app/src/main/java/com/example/pontosrecife/MainActivity.kt
@@ -1,11 +1,49 @@
 package com.example.pontosrecife
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import com.example.pontosrecife.databinding.ActivityMainBinding
 
+/**
+ * Activity principal do programa
+ * Aqui declaramos a existência/inicialização de alguns itens
+ * drawerLayout tem suas propriedades definidas e é inserido como propriedade do appBarConfiguration
+ * appBarConfiguration, por sua vez, permite a definição de algumas páginas como "principais, de maior nível hierárquico", habilitando o ícone do hamburger menu nessas páginas
+ * A função onSupportNavigateUp providencia a funcionalidade do hamburger menu nas páginas principais e o botão de voltar nas secundárias, após o setup do NavigationUI.
+ */
 class MainActivity : AppCompatActivity() {
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        @Suppress("UNUSED_VARIABLE")
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+
+        drawerLayout = binding.drawerLayout
+
+        //drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED)
+
+        val navController = this.findNavController(R.id.myNavHostFragment)
+
+        val topLevelDestinations = setOf(R.id.titleFragment)
+        appBarConfiguration = AppBarConfiguration.Builder(topLevelDestinations)
+                .setOpenableLayout(drawerLayout)
+                .build()
+
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration)
+
+        NavigationUI.setupWithNavController(binding.navView, navController)
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        return NavigationUI.navigateUp(navController, appBarConfiguration)
     }
 }
---
+++ app/src/main/java/com/example/pontosrecife/TitleFragment.kt
@@ -0,0 +1,46 @@
+package com.example.pontosrecife
+
+import android.os.Bundle
+import android.view.*
+import androidx.fragment.app.Fragment
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import com.example.pontosrecife.databinding.FragmentTitleBinding
+
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * Classe responsável pelo comportamento do fragment_title
+ */
+class TitleFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding = DataBindingUtil.inflate<FragmentTitleBinding>(
+            inflater,
+            R.layout.fragment_title, container, false
+        )
+/*
+        binding.iniciarButton.setOnClickListener { view: View ->
+            view.findNavController().navigate(R.id.action_titleFragment_to_bruceFragment)
+        }
+*/
+        setHasOptionsMenu(true)
+        return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.options_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.onNavDestinationSelected(item, requireView().findNavController())
+                || super.onOptionsItemSelected(item)
+    }
+
+}

2afcf3367e97d3a87a8459ea439a58a783735013
João <joao@MacBook-Pro-de-Joao.local>
2021-06-15 20:03:29 -0300

    fragmentos da tela de contato em andamento

---
+++ app/src/main/java/com/example/pontosrecife/ContatoFragment.kt
@@ -0,0 +1,29 @@
+package com.example.pontosrecife
+
+import android.os.Bundle
+import android.view.*
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+
+class ContatoFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        setHasOptionsMenu(true)
+        return inflater.inflate(R.layout.fragment_contato, container, false)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.options_menu,menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.onNavDestinationSelected(item,requireView().findNavController())
+                ||super.onOptionsItemSelected(item)
+    }
+}
